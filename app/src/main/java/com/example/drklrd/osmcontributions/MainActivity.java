9d6b6d132266b19e484c9257cf28ea71bd5da4f1
drklrd <srvbhattarai@gmail.com>
2017-10-14 08:55:02 +0545

    add navigation item listener

--- app/src/main/java/com/example/drklrd/osmcontributions/MainActivity.java
+++ app/src/main/java/com/example/drklrd/osmcontributions/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.drklrd.osmcontributions;
 
 import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
@@ -44,7 +46,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     MaterialSearchView searchView;
 
@@ -62,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
 
 
     public void searchViewCode(){
@@ -159,7 +162,8 @@ public class MainActivity extends AppCompatActivity {
         toggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
 
 
@@ -248,4 +252,21 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.about:
+                Toast.makeText(getApplicationContext(),"About !",Toast.LENGTH_SHORT).show();
+                break;
+            default:
+
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
 }
